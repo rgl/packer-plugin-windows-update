a976d557e15f220f4b1d3adc1c495d2fa6252f7e
Rui Lopes <rgl@ruilopes.com>
2021-07-31 17:32:22 +0100

    configure the ci to ensure that the provisioner.hcl2spec.go file can be generated and is updated

--- ci-release.sh
+++ ci-release.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
+# ensure provisioner.hcl2spec.go is updated by re-generate it. if there are
+# differences, abort the build.
+rm update/provisioner.hcl2spec.go
+make update/provisioner.hcl2spec.go
+git diff --exit-code update/provisioner.hcl2spec.go \
+  || (echo 'ERROR: You must re-generate update/provisioner.hcl2spec.go and commit the changes.' && exit 1)
+
+# do the release.
 if [[ $GITHUB_REF == refs/tags/v* ]]; then
   make release
 else
