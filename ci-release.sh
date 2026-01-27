875de78a501ed254d1fa00f5ef0561273640d56a
Rui Lopes <rgl@ruilopes.com>
2026-01-27 20:08:40 +0000

    sign the release checksum artifact

--- ci-release.sh
+++ ci-release.sh
@@ -8,6 +8,9 @@ make update/provisioner.hcl2spec.go
 git diff --exit-code update/provisioner.hcl2spec.go \
   || (echo 'ERROR: You must re-generate update/provisioner.hcl2spec.go and commit the changes.' && exit 1)
 
+# point the msys2 user gpg home to the one managed by the crazy-max/ghaction-import-gpg github action.
+ln -s /c/Users/runneradmin/.gnupg ~/.gnupg
+
 # do the release.
 if [[ $GITHUB_REF == refs/tags/v* ]]; then
   make release
